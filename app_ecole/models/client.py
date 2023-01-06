15de96f3cd82001a1134f437c9e4c8db2fdfff1e
EmaEmbete <embetemmelelwa@gmail.com>
2023-01-06 23:48:02 +0200

    Creation de la classe modele client

---
+++ app_ecole/models/client.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+
+class Client(models.Model):
+    NomClient = models.CharField(max_length=20)
+    Adress = models.CharField(max_length = 50)
+    Telephone = models.CharField(max_length = 13)
+    Email = models.EmailField(max_length = 100)
+    MotPass = models.CharField(max_length = 32)
+    CodeConfirmation = models.IntegerField()
+
+
+    def __str__(self):
+
+        return self.NomClient
