0375d52f017ad2c0eddd7f5271b2f7aa485cae0a
EmaEmbete <embetemmelelwa@gmail.com>
2023-01-21 11:52:29 +0200

    modifcation valeur default

--- app_ecole/models/codeclient.py
+++ app_ecole/models/codeclient.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class CodeClient(models.Model):
-    code = models.IntegerField(default = 0)
+    code = models.IntegerField()
     montant = models.FloatField(default = 0)
     date = models.DateField(auto_now_add = True)
     etat = models.CharField(max_length = 25, default = "aucun")
